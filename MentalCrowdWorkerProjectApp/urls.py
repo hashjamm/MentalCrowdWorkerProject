b1b61fe556ad39463b066ff8e97af81b26dedd0e
hashjamm <hashjamm@gamil.com>
2025-08-11 16:00:23 +0900

    report 기능 외에 대하여 GET/POST 기능 분리 작업

--- MentalCrowdWorkerProjectApp/urls.py
+++ MentalCrowdWorkerProjectApp/urls.py
@@ -14,27 +14,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-from MentalCrowdWorkerProjectApp.views import BasicInfoAPIView, StressFactorsAPIView, JobSatisfactionAPIView, \
-    JobSatisfactionStressFactorsAPIView, SleepHealthAPIView, GeneralHealthAPIView, EmotionAPIView, LonelinessAPIView, \
-    WholeSurveysAPIView, ReportAPIView
+from django.urls import path
+from MentalCrowdWorkerProjectApp.views import (
+    BasicInfoAPIView, StressFactorsAPIView, JobSatisfactionAPIView, 
+    JobSatisfactionStressFactorsAPIView, SleepHealthGetView, PSQICalculateView, 
+    GeneralHealthGetView, WHODASCalculateView, EmotionGetView, 
+    DASS21CalculateView, LonelinessGetView, LSISCalculateView, 
+    WholeSurveysCalculateView, ReportAPIView
+)
 
 urlpatterns = [
-    path("app_get_basic_info/", BasicInfoAPIView.as_view(), name='get'),
-    path("app_get_stress_factors/", StressFactorsAPIView.as_view(), name='get'),
-    path("app_get_job_satisfaction/", JobSatisfactionAPIView.as_view(), name='get'),
-    path("app_get_job_satisfaction_stress_factors/", JobSatisfactionStressFactorsAPIView.as_view(), name='get'),
-    path("app_get_sleep_health/", SleepHealthAPIView.as_view(), name='get'),
-    path("app_get_general_health/", GeneralHealthAPIView.as_view(), name='get'),
-    path("app_get_emotion/", EmotionAPIView.as_view(), name='get'),
-    path("app_get_loneliness/", LonelinessAPIView.as_view(), name='get'),
-    path("app_calculate_PSQI_K/", SleepHealthAPIView.as_view(), name='post'),
-    path("app_calculate_WHODAS_K/", GeneralHealthAPIView.as_view(), name='post'),
-    path("app_calculate_DASS21_K/", EmotionAPIView.as_view(), name='post'),
-    path("app_calculate_LSIS/", LonelinessAPIView.as_view(), name='post'),
-    path("app_calculate_whole_scores/", WholeSurveysAPIView.as_view(), name='post'),
-    path('report/', ReportAPIView.as_view()),
+    path("app_get_basic_info/", BasicInfoAPIView.as_view(), name='get_basic_info'),
+    path("app_get_stress_factors/", StressFactorsAPIView.as_view(), name='get_stress_factors'),
+    path("app_get_job_satisfaction/", JobSatisfactionAPIView.as_view(), name='get_job_satisfaction'),
+    path("app_get_job_satisfaction_stress_factors/", JobSatisfactionStressFactorsAPIView.as_view(), name='get_job_satisfaction_stress_factors'),
+    path("app_get_sleep_health/", SleepHealthGetView.as_view(), name='get_sleep_health'),
+    path("app_get_general_health/", GeneralHealthGetView.as_view(), name='get_general_health'),
+    path("app_get_emotion/", EmotionGetView.as_view(), name='get_emotion'),
+    path("app_get_loneliness/", LonelinessGetView.as_view(), name='get_loneliness'),
+    path("app_calculate_PSQI_K/", PSQICalculateView.as_view(), name='calculate_psqi'),
+    path("app_calculate_WHODAS_K/", WHODASCalculateView.as_view(), name='calculate_whodas'),
+    path("app_calculate_DASS21_K/", DASS21CalculateView.as_view(), name='calculate_dass21'),
+    path("app_calculate_LSIS/", LSISCalculateView.as_view(), name='calculate_lsis'),
+    path("app_calculate_whole_scores/", WholeSurveysCalculateView.as_view(), name='calculate_whole_surveys'),
+    path('report/', ReportAPIView.as_view(), name='report'),
 ]
